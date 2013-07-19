267a4a095adec407ee02472764d869ef826b55c1
Tiago Fernandes <tjg.fernandes@gmail.com>
2013-07-19 15:31:57 +0100

    removing search=& param from fetch new_uri on response redirect

--- lib/mechanize/http/agent.rb
+++ lib/mechanize/http/agent.rb
@@ -994,6 +994,7 @@ class Mechanize::HTTP::Agent
     new_uri = secure_resolve! response['Location'].to_s, page
 
     @history.push(page, page.uri)
+    new_uri = resolve response['Location'].to_s.gsub("search=&",""), page
 
     params = []
     search_param_regexp = Regexp.new("search=false")
